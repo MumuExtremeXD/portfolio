7bdff3064a6bb20ba851afd225d7a9bce1a6a04f
Matheus Souza <matheus.souza.lima88@gmail.com>
2021-08-06 15:14:45 -0300

    Fix errors deploy 01

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Routes from "./routes";
 
 import GlobalStyles from "./styles/GlobalStyles";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <BrowserRouter>
